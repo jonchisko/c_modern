2a17d983ef7f27418884b8adb5a95604daba9339
Jon Škoberne <jonchisko@gmail.com>
2024-07-10 21:59:33 +0200

    Solving quizzes with the help of come pun

--- chapter2/pun.c
+++ chapter2/pun.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
+  float a, b;
+  // scanf("%f, %f", &a, &b);
+  // printf("%f : %f", a, b);
+  
+  scanf("%f /%f", &a, &b);
+  printf("%f : %f", a, b);
+  // int a;
+  // printf("%d", a);
   printf("To C, or not to C: that is the question.\n");
+  
+  printf("test: %5.5d!\n", 2);
+  printf("test:%15.5e\n", 30.253);
+  printf("test:%.4f\n", 83.162);
+  printf("test:%-6.2g\n", .0000009979);
+
+
+  printf("%d\n", 8 / -5);
+  printf("%d\n", -8 / -5);  
   return 0;
 }
